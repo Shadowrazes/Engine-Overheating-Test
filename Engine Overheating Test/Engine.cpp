3a8367160425d0e2177e3381a363f2f1934ae48c
Михаил <hugehug@mail.ru>
2022-04-07 23:19:32 +0700

    Add the definition of the abstract class Engine

--- Engine Overheating Test/Engine.cpp
+++ Engine Overheating Test/Engine.cpp
@@ -1,1 +1,29 @@
 #include "Engine.h"
+
+using namespace std;
+
+Engine::Engine(float _boost, float _overheatTemp, float _coolingCoef, float _temperature, float _envrmntTemp):
+    boost(_boost), overheatTemp(_overheatTemp), coolingCoef(_coolingCoef), temperature(_temperature), EnvrmntTemp(_envrmntTemp)
+{
+    
+}
+
+Engine::~Engine() {
+
+}
+
+float Engine::GetBoost() {
+    return boost;
+}
+
+float Engine::GetCoolingCoef() {
+    return coolingCoef;
+}
+
+float Engine::GetOverheatTemp() {
+    return overheatTemp;
+}
+
+float Engine::GetTemperature() {
+    return temperature;
+}
--- Engine Overheating Test/Engine.h
+++ Engine Overheating Test/Engine.h
@@ -1,7 +1,25 @@
-#pragma once
+﻿#pragma once
 
-class Engine
-{
+class Engine{ // общий класс для всех двигателей
 
+protected:
+	float boost;  // ускорение
+	float overheatTemp; // температура перегрева
+	float coolingCoef;  // коэффициент охлаждения
+	float temperature;  // температура двигателя
+
+public:
+	float EnvrmntTemp; // температура окружающей среды
+
+	Engine(float _boost, float _overheatTemp, float _coolingCoef, float _temperature, float _envrmntTemp);
+	virtual ~Engine();
+
+	virtual float HeatingRate() = 0; // скорость нагрева
+	virtual float CoolingRate() = 0; // скорость охлаждения
+
+	virtual float GetBoost();
+	virtual float GetOverheatTemp();
+	virtual float GetCoolingCoef();
+	virtual float GetTemperature();
 };
 
