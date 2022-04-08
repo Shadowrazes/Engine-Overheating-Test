3217c6b66a9b58e26a14dea44cfb67836deb4189
Михаил <hugehug@mail.ru>
2022-04-08 22:59:37 +0700

    Define the ICE class

--- Engine Overheating Test/ICE.cpp
+++ Engine Overheating Test/ICE.cpp
@@ -1,1 +1,95 @@
 #include "ICE.h"
+#include <chrono>
+#include <cmath>
+#include <future>
+
+namespace EngineTest {
+	ICE::ICE(double _overheatTemp, double _coolingCoef, double _envrmntTemp, double _inertMoment,
+		std::vector<Linear::LineFunc> _chart, double _coefH_m, double _coefH_v) : Engine(_overheatTemp, _coolingCoef, _envrmntTemp)
+	{
+		isRunning = false;
+		torque = 20;
+		inertMoment = _inertMoment;
+		boost = torque / inertMoment;
+		temperature = _envrmntTemp;
+		chart = _chart;
+		coefH_m = _coefH_m;
+		coefH_v = _coefH_v;
+	}
+
+	double ICE::HeatingRate() {
+		return inertMoment * coefH_m + std::pow(crankshaftSpeed, 2) * coefH_v;
+	}
+
+	double ICE::CoolingRate() {
+		return coolingCoef * (EnvrmntTemp - temperature);
+	}
+
+	double ICE::FindTorque(const double& x) {
+		int l = 0;
+		int r = chart.size() - 1;
+		int mid;
+		while ((l <= r) ) {
+			mid = (l + r) / 2;
+			if (x >= chart[mid].GetStart().X && x <= chart[mid].GetEnd().X)
+				return chart[mid].Func(x);
+			if (chart[mid].GetEnd().X > x) r = mid - 1;
+			else l = mid + 1;
+		}
+		throw std::out_of_range("Going beyond the boundaries of the function");
+	}
+
+	void ICE::EngineOperation() {
+		isRunning = true;
+		int timeout = 199;
+		int timeAfterStart = 0;
+		while (isRunning && timeAfterStart < timeout) {
+			temperatureList.push_back(temperature);
+			temperature += HeatingRate() + CoolingRate();
+			crankshaftSpeed += boost;
+			torque = FindTorque(crankshaftSpeed);
+			boost = torque / inertMoment;
+			timeAfterStart++;
+			std::this_thread::sleep_for(std::chrono::milliseconds(50));
+		}
+		temperatureList.push_back(temperature);
+		isRunning = false;
+	}
+
+	void ICE::Launch() {
+		auto exodus = std::async(&ICE::EngineOperation, this);	// запуск симул€ции и ожидание результата
+		exodus.get();
+	}
+
+	void ICE::ShutDown() {
+		isRunning = false;
+	}
+
+	bool ICE::IsRunning() {
+		return isRunning;
+	}
+
+	double ICE::GetTorque() {
+		return torque;
+	}
+
+	double ICE::GetCrankshaftSpeed() {
+		return crankshaftSpeed;
+	}
+
+	double ICE::GetInertMoment() {
+		return inertMoment;
+	}
+
+	double ICE::GetCoefH_m() {
+		return coefH_m;
+	}
+
+	double ICE::GetCoefH_v() {
+		return coefH_v;
+	}
+
+	std::vector<double> ICE::GetTemperatureList() {
+		return temperatureList;
+	}
+}
--- Engine Overheating Test/ICE.h
+++ Engine Overheating Test/ICE.h
@@ -1,9 +1,41 @@
 #pragma once
 
+#include <vector>
 #include "Engine.h"
+#include "LineFunc.h"
 
-class ICE : protected Engine
-{
+namespace EngineTest {
 
-};
+	class ICE : public Engine {						// ДВС
+
+	protected:
+		bool isRunning;								// статус двигателя
+		double torque;								// текущий крутящий момент
+		double crankshaftSpeed;						// текущая скорость вращения коленвала
+		double inertMoment;							// момент инерции
+		double coefH_m;								// коэффициент зависимости скорости нагрева от крутящего момента
+		double coefH_v;								// коэффициент зависимости скорости нагрева от скорости вращения коленвала
+		std::vector<Linear::LineFunc> chart;		// кусочные функции зависимости крутящего момента от скорости вращения коленвала
+		std::vector<double> temperatureList;		// посекундный отчет температуры
+
+		double HeatingRate();						// скорость нагрева
+		double CoolingRate();						// скорость охлаждения
+		double FindTorque(const double& x);			// бинарный поиск нужной функции и вычисление от нее крутящего момента 
+		void EngineOperation();						// симуляция работы двигателя
+
+	public:
+		ICE(double _overheatTemp, double _coolingCoef, double _envrmntTemp, double _inertMoment,
+			std::vector<Linear::LineFunc> _chart, double _coefH_m, double _coefH_v);
+
+		void Launch();								// запуск
+		void ShutDown();							// остановка
+		bool IsRunning();
+		double GetTorque();
+		double GetCrankshaftSpeed();
+		double GetInertMoment();
+		double GetCoefH_m();
+		double GetCoefH_v();
+		std::vector<double> GetTemperatureList();
+	};
+}
 
