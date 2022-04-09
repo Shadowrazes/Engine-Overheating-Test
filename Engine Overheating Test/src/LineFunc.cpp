0f80ef6b6f3889fa5736ff4c927dc310b44b3f72
Михаил <hugehug@mail.ru>
2022-04-09 12:24:18 +0700

    Add description

--- Engine Overheating Test/src/LineFunc.cpp
+++ Engine Overheating Test/src/LineFunc.cpp
@@ -7,6 +7,6 @@ namespace Linear {
 	}
 
 	double LineFunc::Func(const double& x) {
-		return (x - start.X) * (end.Y - start.Y) / (end.X - start.X) + start.Y; 
+		return (x - start.X) * (end.Y - start.Y) / (end.X - start.X) + start.Y;  // уравнение прямой по двум точкам
 	}
 }
